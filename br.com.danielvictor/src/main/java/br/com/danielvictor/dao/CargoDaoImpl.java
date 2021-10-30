c4b6bc2380288649f1ea725dae45c9d3411fbc99
DanielVictor20 <danielvpcm@gmail.com>
2021-10-30 15:34:19 -0300

    Projeto Programação Web - 2ª aula

---
+++ br.com.danielvictor/src/main/java/br/com/danielvictor/dao/CargoDaoImpl.java
@@ -0,0 +1,10 @@
+package br.com.danielvictor.dao;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.danielvictor.domain.Cargo;
+
+@Repository
+public class CargoDaoImpl extends AbstractDao<Cargo, Long> implements CargoDao{
+
+}
---
+++ br.com.danielvictor/src/main/java/br/com/danielvictor/dao/FuncionarioDao.java
@@ -0,0 +1,19 @@
+package br.com.danielvictor.dao;
+
+import java.util.List;
+
+import br.com.danielvictor.domain.Funcionario;
+
+public interface FuncionarioDao {
+	
+	void save(Funcionario funcionario);
+	
+	void update(Funcionario funcionario);
+	
+	void delete(Long id);
+	
+	Funcionario findById(Long id);
+	
+	List<Funcionario> findAll();
+
+}
